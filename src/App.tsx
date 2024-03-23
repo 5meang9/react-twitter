b3b2ff15eff9dae4ff9ff7f7527f944c0c5986bd
5meang9 <uiop3724@daum.net>
2024-03-23 23:20:57 +0900

    firebase auth 사용 가능하도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+
+import { useState } from 'react';
 import Router from './Router';
 import { Layout } from './components/Layout';
+import { app } from './firebaseApp';
+import { getAuth } from 'firebase/auth';
 
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <Layout>
       <Router/>
@@ -10,3 +19,5 @@ function App() {
 }
 
 export default App;
+
+
