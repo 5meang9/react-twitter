44c619e34bce737fe121fbca82e24802c878aa8a
5meang9 <uiop3724@daum.net>
2024-03-26 13:15:49 +0900

    구글과 깃허브로 sns 로그인 및 회원가입 구현

--- src/components/users/SignupForm.tsx
+++ src/components/users/SignupForm.tsx
@@ -1,33 +1,37 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { app } from "firebaseApp";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
+import { app } from "firebaseApp";
 import { toast } from "react-toastify";
 
-export default function SignupForm(){
+export default function SignupForm() {
   const [error, setError] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
   const navigate = useNavigate();
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: any) => {
     e.preventDefault();
     try {
       const auth = getAuth(app);
       await createUserWithEmailAndPassword(auth, email, password);
-
-      toast.success("회원가입에 성공했습니다.");
       navigate("/");
+      toast.success("성공적으로 회원가입이 되었습니다.");
     } catch (error: any) {
-      console.log(error);
       toast.error(error?.code);
     }
-  }
+  };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const{
-      target: {name, value},
+    const {
+      target: { name, value },
     } = e;
 
     if (name === "email") {
@@ -38,7 +42,7 @@ export default function SignupForm(){
       if (!value?.match(validRegex)) {
         setError("이메일 형식이 올바르지 않습니다.");
       } else {
-        setError(""); 
+        setError("");
       }
     }
 
@@ -46,9 +50,9 @@ export default function SignupForm(){
       setPassword(value);
 
       if (value?.length < 8) {
-        setError("비밀번호는 8자리 이상으로 입력해주세요");
+        setError("비밀번호는 8자리 이상 입력해주세요");
       } else if (value !== passwordConfirmation) {
-        setError("비밀번호와 비밀번호 확인 값이 다릅니다. 다시 확인해주세요.");
+        setError("비밀번호와 비밀번호 확인 값이 다릅니다.");
       } else {
         setError("");
       }
@@ -58,42 +62,124 @@ export default function SignupForm(){
       setPasswordConfirmation(value);
 
       if (value?.length < 8) {
-        setError("비밀번호는 8자리 이상으로 입력해주세요");
+        setError("비밀번호는 8자리 이상 입력해주세요");
       } else if (value !== password) {
-        setError("비밀번호와 비밀번호 확인 값이 다릅니다. 다시 확인해주세요.");
+        setError("비밀번호와 비밀번호 확인 값이 다릅니다.");
       } else {
         setError("");
       }
     }
-  }
+  };
 
-  return(
+  const onClickSocialLogin = async (e: any) => {
+    const {
+      target: { name },
+    } = e;
+
+    let provider;
+    const auth = getAuth(app);
+
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    }
+
+    if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+
+    await signInWithPopup(
+      auth,
+      provider as GithubAuthProvider | GoogleAuthProvider
+    )
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+        toast.success("로그인 되었습니다.");
+      })
+      .catch((error) => {
+        console.log(error);
+        const errorMessage = error?.message;
+        toast?.error(errorMessage);
+      });
+  };
+
+  return (
     <form className="form form--lg" onSubmit={onSubmit}>
       <div className="form__title">회원가입</div>
       <div className="form__block">
         <label htmlFor="email">이메일</label>
-        <input type="text" name="email" id="email" value={email} required onChange={onChange} />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          required
+          onChange={onChange}
+        />
       </div>
       <div className="form__block">
         <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" required value={password} onChange={onChange} />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
       </div>
       <div className="form__block">
         <label htmlFor="password_confirmation">비밀번호 확인</label>
-        <input type="password" name="password_confirmation" id="password_confirmation" required value={passwordConfirmation} onChange={onChange} />
+        <input
+          type="password"
+          name="password_confirmation"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={onChange}
+          required
+        />
       </div>
       {error && error?.length > 0 && (
         <div className="form__block">
           <div className="form__error">{error}</div>
         </div>
       )}
+
       <div className="form__block">
         계정이 있으신가요?
-        <Link to="/users/login" className="form__link">로그인하기</Link>
+        <Link to="/users/login" className="form__link">
+          로그인하기
+        </Link>
       </div>
       <div className="form__block--lg">
-        <button type="submit" className="form__btn--submit" disabled={error?.length > 0}>회원가입</button>
+        <button
+          type="submit"
+          className="form__btn--submit"
+          disabled={error?.length > 0}
+        >
+          회원가입
+        </button>
+      </div>
+      <div className="form__block">
+        <button
+          type="button"
+          name="google"
+          className="form__btn--google"
+          onClick={onClickSocialLogin}
+        >
+          Google로 회원가입
+        </button>
+      </div>
+      <div className="form__block">
+        <button
+          type="button"
+          name="github"
+          className="form__btn--github"
+          onClick={onClickSocialLogin}
+        >
+          Github으로 회원가입
+        </button>
       </div>
     </form>
-  )
+  );
 }
