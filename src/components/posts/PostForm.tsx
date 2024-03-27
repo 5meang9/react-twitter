c5c42c0f58b025975b9f04ede417fff00ecc61e9
5meang9 <uiop3724@daum.net>
2024-03-27 14:04:32 +0900

    1. 해시태그 firestore addDoc 추가 2. 해시태그 css 추가 3. 해시태그 리스트 노출 구현

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -8,11 +8,26 @@ import AuthContext from "context/AuthContext";
 
 export default function PostForm(){
   const [content, setContent] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
+  const [ hashTag, setHashTag] = useState<string>('');
   const {user} = useContext(AuthContext);
 
   const handleFileUpload = () =>{
 
   };
+
+  const handleKeyUp = (e: any) => {
+    if(e.keyCode === 32 && e.target.value.trim() !== ''){
+      // 만약 같은 태그가 잇다면 에러를 띄운다.
+      // 태그를 생성해준다.
+      if(tags?.includes(e.target.value?.trim())){
+        toast.error('같은 태그가 있습니다.');
+      }else{
+        setTags((prev) => (prev?.length > 0 ? [...prev, hashTag] : [hashTag]));
+        setHashTag('');
+      }
+    }
+  }
 
   const onSubmit = async(e: any) =>{
     e.preventDefault();
@@ -27,7 +42,10 @@ export default function PostForm(){
         }),
         uid: user?.uid,
         email: user?.email,
+        hashTags: tags,
       });
+      setTags([]);
+      setHashTag('');
       setContent("");
       toast.success("게시글을 생성했습니다.");
     } catch (e: any) {
@@ -45,6 +63,14 @@ export default function PostForm(){
     }
   };
 
+  const removeTag = (tag: string) =>{
+    setTags(tags?.filter((val) => val !== tag));
+  }
+
+  const onChangeHashTag = (e: any) => {
+    setHashTag(e?.target?.value?.trim());
+  }
+
   return(
     <form className="post-form" onSubmit={onSubmit}>
       <textarea 
@@ -56,6 +82,23 @@ export default function PostForm(){
         onChange={onChange}
         value={content}
       />
+      <div className="post-form__hashtags">
+        <span className="post-form__hashtags-outputs">
+          {tags?.map((tag, index) =>(
+            <span className="post-form__hashtags-tag" key={index} onClick={() => removeTag(tag)}>#{tag}</span>
+          ))}
+        </span>
+        <input 
+          type="text" 
+          className="post-form__input"  
+          name="hashtag" 
+          id="hashtag" 
+          placeholder="해시태그 + 스페이스바 입력"
+          onChange={onChangeHashTag}
+          onKeyUp={handleKeyUp}
+          value={hashTag}
+        />
+      </div>
       <div className="post-form__submit-area">
         <label htmlFor="file-input" className='post-form__file'>
           <FiImage className='post-form__file-icon'/>
