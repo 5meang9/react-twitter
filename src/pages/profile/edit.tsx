8a8adecf56c848cbddb10138ac093b761993b948
5meang9 <uiop3724@daum.net>
2024-04-01 20:12:22 +0900

    기존 유저 이미지가 Firebase Storage 이미지 삭제 및 프로필 scss 수정

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -9,6 +9,8 @@ import { updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
+const STORAGE_DOWNLOAD_URL_STR = "https://firebasestorage.googleapis.com";
+
 export default function ProfileEdit(){
   const [displayName, setDisplayName] = useState<string >('');
   const [imageUrl, setImageUrl] = useState<string | null>(null);
@@ -31,13 +33,15 @@ export default function ProfileEdit(){
     e.preventDefault();
 
     try {
-      // 기존 이미지 삭제
-      // if (user?.photoURL){
-      //   const imageRef = ref(storage, user?.photoURL);
-      //   await deleteObject(imageRef).catch((error) => {
-      //     console.log(error);
-      //   });
-      // }
+      // 기존 유저 이미지가 Firebase Storage 이미지 삭제
+      if (user?.photoURL && user?.photoURL?.includes(STORAGE_DOWNLOAD_URL_STR)){
+        const imageRef = ref(storage, user?.photoURL);
+        if(imageRef){
+          await deleteObject(imageRef).catch((error) => {
+            console.log(error);
+          });
+        }
+      }
       //이미지 업로드
       if(imageUrl){
         const data = await uploadString(storageRef, imageUrl, "data_url");
