1f8ce80c87a52e1762e49609402df9dff1bc69e6
5meang9 <uiop3724@daum.net>
2024-04-02 14:52:40 +0900

    팔로잉과 팔로우 기능 구현 및 레이아웃 작성, firebase query 이용하여 filter 되도록 변경

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -9,6 +9,7 @@ import { db } from "firebaseApp";
 import { toast } from "react-toastify";
 import { getStorage, ref, deleteObject } from "firebase/storage";
 import { storage } from "firebaseApp";
+import FollowingBox from "components/comments/following/FollowingBox";
 
 interface PostBoxProps{
   post: PostProps;
@@ -58,13 +59,18 @@ export default function PostBox({post}: PostBoxProps){
   
   return(
     <div className='post__box' key={post?.id}>
-      <Link to={`/posts/${post?.id}`}>
-        <div className='post__box-profile'>
-          <div className='post__flex'>
-            {post?.profileUrl ? <img src={post?.profileUrl} alt='profile' className='post__box-profile-img' /> : <FaUserCircle className='post__box-profile-icon' />}
-            <div className='post__eamil'>{post?.email}</div>
-            <div className='post__createdAt'>{post?.createdAt}</div>
+      <div className='post__box-profile'>
+        <div className='post__flex'>
+          {post?.profileUrl ? <img src={post?.profileUrl} alt='profile' className='post__box-profile-img' /> : <FaUserCircle className='post__box-profile-icon' />}
+          <div className="post__flex--between">
+            <div className="post__flex">
+              <div className='post__eamil'>{post?.email}</div>
+              <div className='post__createdAt'>{post?.createdAt}</div>
+            </div>
+            <FollowingBox post={post} />
           </div>
+        </div>
+        <Link to={`/posts/${post?.id}`}>
           <div className='post__box-content'>{post?.content}</div>
           {post?.imageUrl && (
             <div className="post__image-div">
@@ -76,8 +82,8 @@ export default function PostBox({post}: PostBoxProps){
               <span className="post-form__hashtags-tag" key={index}>#{tag}</span>
             ))}
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
       <div className='post__box-footer'>
         {user?.uid === post?.uid && (
           <>
